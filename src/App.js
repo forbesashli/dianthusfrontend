c1516120f95686068a94d9abfa13bfb650c1202d
forbesashli <44144720+forbesashli@users.noreply.github.com>
2020-03-07 21:55:57 -0800

    fourth commit(okay)

--- src/App.js
+++ src/App.js
@@ -4,12 +4,11 @@ import './App.css';
 
 // Firebase App (the core Firebase SDK) is always required and
 // must be listed before other Firebase SDKs
-import * as firebase from "firebase/app";
+var firebase = require("firebase/app");
 
-// Add the Firebase services that you want to use
-import "firebase/auth";
-import "firebase/firestore";
-
+// Add the Firebase products that you want to use
+require("firebase/auth");
+require("firebase/firestore");
 function App() {
   return (
     <div className="App">
