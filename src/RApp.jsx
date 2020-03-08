9584ed2b2a4864d3cf9424b1f6766556e571aef3
emilyychenn <59349980+emilyychenn@users.noreply.github.com>
2020-03-07 20:14:14 -0800

    basic react app created

---
+++ src/RApp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class RApp extends React.Component {
+    render() {
+        return(
+            <React.Fragment>
+            <h1>Hello World</h1>
+            <div>Welcome, user.</div>
+            </React.Fragment>
+        );
+    }
+}
